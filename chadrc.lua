67a7c4fe7c419e5ee9bddb1ad4b405f76f8bbe09
Wille Faler <wfaler@chaordic.io>
2024-04-30 20:48:31 +0200

    autoformat ts/js files with prettier on save

--- chadrc.lua
+++ chadrc.lua
@@ -4,8 +4,11 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
         command = "set filetype=templ",
 })
 -- invoke prettier on save for js, ts etc files
+
 vim.api.nvim_create_user_command('Prettier', function()
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)  -- Save cursor position
     vim.cmd('%!prettier --stdin-filepath ' .. vim.fn.shellescape(vim.api.nvim_buf_get_name(0)))
+    vim.api.nvim_win_set_cursor(0, cursor_pos)  -- Restore cursor position
 end, {})
 
 vim.api.nvim_create_augroup("fmt", { clear = true })
